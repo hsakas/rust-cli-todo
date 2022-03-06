a1350aa70c1a5c6d4d7264739f8f35202f504c64
Akash <akashofficial176@gmail.com>
2022-03-06 13:17:26 +0000

    testing rust vectors

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,26 @@
-mod Todo;
-pub use Todo::Todo as Todo;
-pub use Todo::manager as manager;
+mod utils;
+use utils::todo::Todo as Todo;
+
+fn fake_todo() -> Vec<Todo>{
+    let mut todos = Vec::new();
+    todos.push(Todo::new(1, String::from("Buy milk")));
+    todos.push(Todo::new(2, String::from("Buy bread")));
+    todos.push(Todo::new(3, String::from("Buy coffee")));
+    todos
+}
 
 
-fn main() {
+fn main(){
+    let mut todos: Vec<Todo> = fake_todo();
+
+    todos.push(Todo::new(4, String::from("Buy milk")));
+    for todo in todos.iter(){
+        todo.describe(true);
+    }
+
+    todos.append(&mut fake_todo());
+    for todo in todos.iter(){
+        todo.describe(true);
+    }
+
 }
--- src/todoer/mod.rs
+++
@@ -1,3 +0,0 @@
-mod todo;
-pub use todo::Todo as Todo;
-pub use todo::manager as manager;
