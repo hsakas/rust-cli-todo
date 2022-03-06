151eb3cad3dbae34adf56a80862b11bce11f26fd
Akash <akashofficial176@gmail.com>
2022-03-06 13:48:27 +0000

    added todo manager

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,24 @@
 mod utils;
-use utils::todo::Todo as Todo;
-
-fn fake_todo() -> Vec<Todo>{
-    let mut todos = Vec::new();
-    todos.push(Todo::new(1, String::from("Buy milk")));
-    todos.push(Todo::new(2, String::from("Buy bread")));
-    todos.push(Todo::new(3, String::from("Buy coffee")));
-    todos
-}
-
+use utils::todo::{Todo, TodoList};
 
 fn main(){
-    let mut todos: Vec<Todo> = fake_todo();
+    let mut todo_list = TodoList::new();
+    todo_list.add(Todo::new(1, String::from("Buy milk")));
+    todo_list.add(Todo::new(2, String::from("Buy eggs")));
+    todo_list.add(Todo::new(3, String::from("Buy bread")));
+    todo_list.add(Todo::new(4, String::from("Buy coffee")));
+    todo_list.add(Todo::new(5, String::from("Buy tea")));
+    todo_list.add(Todo::new(6, String::from("Buy chocolate")));
+    todo_list.add(Todo::new(7, String::from("Buy milk")));
+    todo_list.add(Todo::new(8, String::from("Buy eggs")));
+    todo_list.add(Todo::new(9, String::from("Buy bread")));
 
-    todos.push(Todo::new(4, String::from("Buy milk")));
-    for todo in todos.iter(){
-        todo.describe(true);
-    }
-
-    todos.append(&mut fake_todo());
-    for todo in todos.iter(){
-        todo.describe(true);
-    }
-
+    todo_list.describe(true);
+    todo_list.toggle(1);
+    todo_list.high(1);
+    todo_list.remove(9);
+    todo_list.remove(2);
+    print!("\n");
+    todo_list.describe(true);
+    
 }
--- src/utils/todo.rs
+++ src/utils/todo.rs
@@ -96,3 +96,141 @@ impl Todo {
         println!("{}", description);
     }
 }
+
+pub struct TodoList{
+    todos: Vec<Todo>,
+}
+
+impl TodoList {
+    pub fn new() -> Self {
+        Self {
+            todos: Vec::new(),
+        }
+    }
+
+    pub fn add(&mut self, todo: Todo) {
+        self.todos.push(todo);
+    }
+
+    pub fn describe(&self, verbose: bool) {
+        for todo in self.todos.iter() {
+            todo.describe(verbose);
+        }
+    }
+
+    pub fn toggle(&mut self, id: i32) {
+        for todo in self.todos.iter_mut() {
+            if todo.id == id {
+                todo.toggle();
+            }
+        }
+    }
+
+    pub fn high(&mut self, id: i32) {
+        for todo in self.todos.iter_mut() {
+            if todo.id == id {
+                todo.high();
+            }
+        }
+    }
+
+    pub fn medium(&mut self, id: i32) {
+        for todo in self.todos.iter_mut() {
+            if todo.id == id {
+                todo.medium();
+            }
+        }
+    }
+
+    pub fn low(&mut self, id: i32) {
+        for todo in self.todos.iter_mut() {
+            if todo.id == id {
+                todo.low();
+            }
+        }
+    }
+
+    pub fn remove(&mut self, id: i32) {
+        self.todos.retain(|todo| todo.id != id);
+    }
+
+    pub fn clear(&mut self) {
+        self.todos.clear();
+    }
+
+    pub fn filter(&self, tag: &str) -> Vec<&Todo> {
+        self.todos.iter().filter(|todo| todo.tag == tag).collect()
+    }
+
+    pub fn filter_completed(&self) -> Vec<&Todo> {
+        self.todos.iter().filter(|todo| todo.completed).collect()
+    }
+
+    pub fn filter_incomplete(&self) -> Vec<&Todo> {
+        self.todos.iter().filter(|todo| !todo.completed).collect()
+    }
+
+    pub fn filter_priority(&self, priority: i32) -> Vec<&Todo> {
+        self.todos.iter().filter(|todo| todo.priority.value == priority).collect()
+    }
+
+    pub fn filter_tag(&self, tag: &str) -> Vec<&Todo> {
+        self.todos.iter().filter(|todo| todo.tag == tag).collect()
+    }
+
+    pub fn filter_text(&self, text: &str) -> Vec<&Todo> {
+        self.todos.iter().filter(|todo| todo.text.contains(text)).collect()
+    }
+
+    pub fn filter_completed_text(&self, text: &str) -> Vec<&Todo> {
+        self.todos.iter().filter(|todo| todo.completed && todo.text.contains(text)).collect()
+    }
+
+    pub fn filter_incomplete_text(&self, text: &str) -> Vec<&Todo> {
+        self.todos.iter().filter(|todo| !todo.completed && todo.text.contains(text)).collect()
+    }
+
+    pub fn filter_priority_text(&self, priority: i32, text: &str) -> Vec<&Todo> {
+        self.todos.iter().filter(|todo| todo.priority.value == priority && todo.text.contains(text)).collect()
+    }
+
+    pub fn filter_tag_text(&self, tag: &str, text: &str) -> Vec<&Todo> {
+        self.todos.iter().filter(|todo| todo.tag == tag && todo.text.contains(text)).collect()
+    }
+
+    pub fn filter_completed_priority_text(&self, priority: i32, text: &str) -> Vec<&Todo> {
+        self.todos.iter().filter(|todo| todo.completed && todo.priority.value == priority && todo.text.contains(text)).collect()
+    }
+
+    pub fn filter_incomplete_priority_text(&self, priority: i32, text: &str) -> Vec<&Todo> {
+        self.todos.iter().filter(|todo| !todo.completed && todo.priority.value == priority && todo.text.contains(text)).collect()
+    }
+
+    pub fn filter_tag_priority_text(&self, tag: &str, priority: i32, text: &str) -> Vec<&Todo> {
+        self.todos.iter().filter(|todo| todo.tag == tag && todo.priority.value == priority && todo.text.contains(text)).collect()
+    }
+
+    pub fn filter_completed_tag_text(&self, tag: &str, text: &str) -> Vec<&Todo> {
+        self.todos.iter().filter(|todo| todo.completed && todo.tag == tag && todo.text.contains(text)).collect()
+    }
+
+    pub fn filter_incomplete_tag_text(&self, tag: &str, text: &str) -> Vec<&Todo> {
+        self.todos.iter().filter(|todo| !todo.completed && todo.tag == tag && todo.text.contains(text)).collect()
+    }
+
+    pub fn filter_priority_tag_text(&self, priority: i32, tag: &str, text: &str) -> Vec<&Todo> {
+        self.todos.iter().filter(|todo| todo.priority.value == priority && todo.tag == tag && todo.text.contains(text)).collect()
+    }
+
+    pub fn filter_completed_priority_tag_text(&self, priority: i32, tag: &str, text: &str) -> Vec<&Todo> {
+        self.todos.iter().filter(|todo| todo.completed && todo.priority.value == priority && todo.tag == tag && todo.text.contains(text)).collect()
+    }
+
+    pub fn filter_incomplete_priority_tag_text(&self, priority: i32, tag: &str, text: &str) -> Vec<&Todo> {
+        self.todos.iter().filter(|todo| !todo.completed && todo.priority.value == priority && todo.tag == tag && todo.text.contains(text)).collect()
+    }
+
+    pub fn filter_completed_tag_priority_text(&self, tag: &str, priority: i32, text: &str) -> Vec<&Todo> {
+        self.todos.iter().filter(|todo| todo.completed && todo.tag == tag && todo.priority.value == priority && todo.text.contains(text)).collect()
+    }
+}
